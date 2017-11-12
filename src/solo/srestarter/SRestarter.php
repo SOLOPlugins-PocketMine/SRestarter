db66454c9fd470b0b6dcd06b36635866e39489cd
solo5star <solo_5@naver.com>
2017-11-12 20:20:00 +0900

    Config->Setting and some type hint

--- src/solo/srestarter/SRestarter.php
+++ src/solo/srestarter/SRestarter.php
@@ -11,11 +11,14 @@ use pocketmine\utils\Utils;
 
 class SRestarter extends PluginBase{
 
+	/** @var Config */
+	private $setting;
+
 	public function onEnable(){
 		@mkdir($this->getDataFolder());
 		$this->saveResource("setting.yml");
 
-		$this->config = new Config($this->getDataFolder() . "setting.yml", Config::YAML);
+		$this->setting = new Config($this->getDataFolder() . "setting.yml", Config::YAML);
 
 		$this->getServer()->getScheduler()->scheduleRepeatingTask(new class($this) extends PluginTask{
 			public function onRun($currentTick){
@@ -24,15 +27,15 @@ class SRestarter extends PluginBase{
 		}, 60);
 	}
 
-	public function check(){
-		$rebootElappsedMinute = $this->config->getNested("reboot-schedule.server-elappsed-minute", PHP_INT_MAX);
+	public function check() : bool{
+		$rebootElappsedMinute = $this->setting->getNested("reboot-schedule.server-elappsed-minute", PHP_INT_MAX);
 		$serverElappsedMinute = (microtime(true) - \pocketmine\START_TIME) / 60;
 		if($serverElappsedMinute > $rebootElappsedMinute){
 			$this->restart();
 			return true;
 		}
 
-		$rebootDateTimes = $this->config->getNested("reboot-schedule.date-time", []);
+		$rebootDateTimes = $this->setting->getNested("reboot-schedule.date-time", []);
 		foreach(is_array($rebootDateTimes) ? $rebootDateTimes : [$rebootDateTimes] as $rebootDateTime){
 			if(abs(time() - strtotime($rebootDateTime)) < 20){
 				$this->restart();
@@ -40,7 +43,7 @@ class SRestarter extends PluginBase{
 			}
 		}
 
-		$rebootMemory = $this->config->getNested("reboot-schedule.over-memory", PHP_INT_MAX);
+		$rebootMemory = $this->setting->getNested("reboot-schedule.over-memory", PHP_INT_MAX);
 		$currentMemory = (Utils::getMemoryUsage(true)[1] / 1024) / 1024;
 		if($rebootMemory < $currentMemory){
 			$this->restart();
@@ -49,20 +52,24 @@ class SRestarter extends PluginBase{
 		return false;
 	}
 
+	public function getSetting() : Config{
+		return $this->setting;
+	}
+
 	public function restart() : bool{
 		static $triggered = false;
 		if($triggered === true){
 			return false;
 		}
 
-		$preBroadcast = $this->config->getNested("reboot-message.pre-broadcast", "");
+		$preBroadcast = $this->setting->getNested("reboot-message.pre-broadcast", "");
 		$preBroadcast = is_array($preBroadcast) ? implode("\n", $preBroadcast) : $preBroadcast;
 
 		$this->getServer()->broadcastMessage($preBroadcast);
 
 		$this->getServer()->getScheduler()->scheduleDelayedTask(new class($this) extends PluginTask{
 			public function onRun(int $currentTick){
-				$kickMessage = $this->owner->getConfig()->getNested("reboot-message.kick-message", "");
+				$kickMessage = $this->owner->getSetting()->getNested("reboot-message.kick-message", "");
 				$kickMessage = is_array($kickMessage) ? implode("\n", $kickMessage) : $kickMessage;
 
 				foreach($this->owner->getServer()->getOnlinePlayers() as $player){
